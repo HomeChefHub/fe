8a004be332d3ec17a07768f2fa0c8f6c1f327d13
Jaminggu <sjmim0220@gmail.com>
2024-07-17 11:28:16 +0900

    [chore] 색상 변수 설정

---
+++ src/constants/Colors.js
@@ -0,0 +1,21 @@
+export const Colors = {
+  brand: {
+    primary: '#00BCD4',
+    secondary: '#CCCCFF',
+  },
+  icon: {
+    primary: '#000000',
+    focused: '#00BCD4'
+  },
+  border: {
+    primary: '#C2C9CE',
+    secondary: '#DBDEE2',
+  },
+  text: {
+    primary: '#000000',
+  },
+  bg: {
+    primary: '#FFFFFF',
+    secondary: '#EAECEF',
+  }
+};
---
+++ src/screens/Fridge/index.jsx
@@ -0,0 +1,7 @@
+import {View} from "react-native";
+
+export default function FridgeScreen() {
+  return (
+    <View></View>
+  );
+}
