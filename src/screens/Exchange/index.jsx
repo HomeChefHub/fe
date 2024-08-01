e327bf5bb56a315729bd74fc078d079dfe1008fe
Jaminggu <sjmim0220@gmail.com>
2024-08-01 16:33:51 +0900

    [feat] 식재료 교환 게시판 메인화면 리스트 무한 스크롤 구현 #4

--- src/screens/Exchange/index.jsx
+++ src/screens/Exchange/index.jsx
@@ -1,9 +1,8 @@
-import { ScrollView, View } from "react-native";
+import { FlatList, View } from "react-native";
 import { globalStyles } from "../../constants/global";
 import { CustomHeader } from "../../components/CustomHeader";
 import { CustomSearchInput } from "../../components/CustomSearchInput";
 import { CustomRowCard } from "../../components/CustomRowCard";
-
 import { CustomAddButton } from "../../components/CustomAddButton";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -11,14 +10,30 @@ import { handleDateFormat } from "../../services/handleDateFormat";
 
 export default function ExchangeScreen({ navigation }) {
   const [exchangeList, setExchangeList] = useState([]);
+  const [lastExchangeId, setLastExchangeId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLastPage, setIsLastPage] = useState(false);
 
   const fetchExchangeList = async () => {
+    if (isLoading || isLastPage) return;
+    setIsLoading(true);
+
     try {
-      const res = await axios.get("http://localhost:8080/api/v1/exchanges");
-      setExchangeList(res.data.content);
-      // setIsLoading(false);
+      const params = lastExchangeId ? { lastExchangeId } : {};
+      const res = await axios.get("http://localhost:8080/api/v1/exchanges", {
+        params,
+      });
+      setExchangeList([...exchangeList, ...res.data.content]);
+      if (res.data.content.length) {
+        setLastExchangeId(
+          res.data.content[res.data.content.length - 1].exchangeId,
+        );
+      }
+      setIsLastPage(res.data.last);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -30,22 +45,25 @@ export default function ExchangeScreen({ navigation }) {
     <View style={globalStyles.container}>
       <CustomHeader title={"식재료 교환 게시판"} />
       <CustomSearchInput placeholder={"찾으시는 재료가 있나요?"} />
-      <ScrollView>
-        {exchangeList.map((exchange) => (
+      <FlatList
+        data={exchangeList}
+        keyExtractor={(item) => item.exchangeId.toString()}
+        renderItem={({ item }) => (
           <CustomRowCard
-            key={exchange.exchangeId}
-            id={exchange.exchangeId}
-            uri={exchange.uri}
-            title={exchange.title}
-            location={exchange.region + " " + exchange.childRegion}
-            date={handleDateFormat(exchange.createDate)}
-            status={exchange.status}
+            id={item.exchangeId}
+            uri={item.uri}
+            title={item.title}
+            location={item.region + " " + item.childRegion}
+            date={handleDateFormat(item.createDate)}
+            status={item.status}
             onPress={() =>
-              navigation.navigate("ExchangeDetail", { id: exchange.exchangeId })
+              navigation.navigate("ExchangeDetail", { id: item.exchangeId })
             }
           />
-        ))}
-      </ScrollView>
+        )}
+        onEndReached={fetchExchangeList}
+        onEndReachedThreshold={0.1}
+      />
       <View>
         <CustomAddButton
           text={"+ 추가"}
