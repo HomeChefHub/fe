9dc7d6951c0289d212438f2f313f5ab9b83e5405
Jaminggu <sjmim0220@gmail.com>
2024-08-16 12:50:21 +0900

    [feat] 홈화면 비로그인 시 랜덤레시피 받아옴 #2

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,17 +1,51 @@
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, View } from "react-native";
 import { CustomHeader } from "../../components/CustomHeader";
 import { globalStyles } from "../../constants/global";
 import { CustomSearchInput } from "../../components/CustomSearchInput";
 import HomeTopTabBar from "../../navigations/HomeTopTabBar";
+import { RecipeCard } from "./_components/RecipeCard";
+import axios from "axios";
 
 export default function HomeScreen() {
+  const [recipeList, setRecipeList] = useState([]);
+  const [memberId, setMemberId] = useState(1);
+
+  const fetchRecipes = async () => {
+    try {
+      const res = await axios.get(`${process.env.API_URL}/recipes/random`, {
+        params: { page: 0, size: 5 },
+      });
+      setRecipeList(res.data.content);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
   return (
     <View style={globalStyles.container}>
       <CustomHeader title={"Refooding"} />
       <CustomSearchInput
         placeholder={"요리명 또는 재료로 레시피를 검색해보세요!"}
       />
-      <HomeTopTabBar />
+      {memberId ? (
+        <HomeTopTabBar />
+      ) : (
+        <ScrollView>
+          {recipeList.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              id={recipe.id}
+              img={recipe.imgSrc}
+              name={recipe.name}
+            />
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 }
