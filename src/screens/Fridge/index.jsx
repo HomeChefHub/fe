1d81f5f0e078c7092c6c64a79fd32fd22797b6cc
Jaminggu <sjmim0220@gmail.com>
2024-08-28 12:54:23 +0900

    [refactor] 내 냉장고 - 메인화면에서 변경된 API에 따라서 코드 변경 #16

--- src/screens/Fridge/index.jsx
+++ src/screens/Fridge/index.jsx
@@ -1,24 +1,22 @@
-import { FlatList, View, StyleSheet } from "react-native";
+import { FlatList, View } from "react-native";
 import { globalStyles } from "../../constants/global";
 import { CustomHeader } from "../../components/CustomHeader";
 import { CustomSearchInput } from "../../components/CustomSearchInput";
 import { CustomAddButton } from "../../components/CustomAddButton";
 import axios from "axios";
-import { useCallback, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { FridgeCard } from "./_components/FridgeCard";
 
 export default function FridgeScreen() {
+  const api_url = process.env.API_URL;
   const [fridgeList, setFridgeList] = useState([]);
-  const id = 1;
   const navigation = useNavigation();
 
   const fetchFridgeList = async () => {
     try {
-      const res = await axios.get(
-        `${process.env.API_URL}/refrigerator/members/${id}`,
-      );
-      setFridgeList(res.data);
+      const res = await axios.get(`${api_url}/ingredients`);
+      setFridgeList(res.data.content);
     } catch (error) {
       console.log(error);
     }
@@ -30,16 +28,12 @@ export default function FridgeScreen() {
     }, []),
   );
 
-  const handleDelete = async (itemId) => {
+  const handleDelete = async (ingredientId) => {
     try {
-      await axios.delete(`${process.env.API_URL}/refrigerator/${itemId}`, {
-        data: {
-          memberId: id,
-        },
-      });
+      await axios.delete(`${api_url}/ingredients/${ingredientId}`);
       fetchFridgeList();
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -51,7 +45,7 @@ export default function FridgeScreen() {
         data={fridgeList}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <FridgeCard memberId={id} item={item} onDelete={handleDelete} />
+          <FridgeCard item={item} onDelete={handleDelete} />
         )}
       />
       <View>
@@ -64,10 +58,3 @@ export default function FridgeScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  itemContainer: {
-    position: "relative",
-    marginBottom: 10,
-  },
-});
